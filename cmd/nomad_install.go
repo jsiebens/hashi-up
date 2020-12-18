6ecdcfa7b5b7d846879c6071bfeef0083d089d9c
Johan Siebens <johan.siebens@gmail.com>
2020-12-18 16:27:07 +0100

    Use releases.hashicorp.com to get the latest version
    Signed-off-by: Johan Siebens <johan.siebens@gmail.com>

--- cmd/nomad_install.go
+++ cmd/nomad_install.go
@@ -98,13 +98,13 @@ func InstallNomadCommand() *cobra.Command {
 		}
 
 		if len(binary) == 0 && len(version) == 0 {
-			versions, err := config.GetVersion()
+			latest, err := config.GetLatestVersion("nomad")
 
 			if err != nil {
 				return errors.Wrapf(err, "unable to get latest version number, define a version manually with the --version flag")
 			}
 
-			version = versions.Nomad
+			version = latest
 		}
 
 		callback := func(op operator.CommandOperator) error {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jsiebens/hashi-up
 go 1.13
 
 require (
+	github.com/Masterminds/semver v1.5.0
 	github.com/alexellis/go-execute v0.0.0-20200124154445-8697e4e28c5e
 	github.com/bramvdbogaerde/go-scp v0.0.0-20200820121624-ded9ee94aef5
 	github.com/hashicorp/hcl/v2 v2.8.0
